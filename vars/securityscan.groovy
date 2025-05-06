0107e04c19f92062bdc4a72d26229134e8e76e2d
Vivek_IGS <145560190+vivek28058@users.noreply.github.com>
2025-05-06 16:58:12 +0530

    Update securityscan.groovy

--- vars/securityscan.groovy
+++ vars/securityscan.groovy
@@ -1,5 +1,5 @@
 def call(String dockerUsername, String dockerPassword, String imageName, String reportDir = "reports") {
     stage('Security Scan (Python)') {
-        bat "python scripts/security_scan.py ${dockerUsername} ${dockerPassword} ${imageName} ${reportDir}"
+        bat "\"C:\\Users\\Vivek_S\\AppData\\Local\\Programs\\Python\\Python313\\python.exe\" scripts/security_scan.py ${dockerUsername} ${dockerPassword} ${imageName} ${reportDir}"
     }
 }
